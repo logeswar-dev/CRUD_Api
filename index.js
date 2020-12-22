191d11b6172eeb2b8f1f4c643536a539c324f42f
logeshdeveloper <logeshdeveloper8@gmail.com>
2020-12-22 13:18:33 +0530

    node and express port are set

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.get('/' , (req , res) => {
+    console.log("Test!")
+
+    res.send("Hello From HomePage")
+}
+)
+
+app.listen(PORT, () => {
+    console.log("Server Running on the Port http://localhost:" + PORT)
+})
