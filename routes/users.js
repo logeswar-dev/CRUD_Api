9aea18382de5c891ecebc596ba41b6b06209c4de
logeshdeveloper <logeshdeveloper8@gmail.com>
2020-12-25 12:18:44 +0530

    delete request has been added

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,40 @@
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router()
 
-const users = [
-    {
-        name : "Logeswar",
-        age : "21"
-    }
-]
+let users = []
 
 router.get('/' , (req , res) => {
     res.send(users)
 })
 
 router.post('/' , (req , res) => {
-    res.send("Request Recieved")
+    const user = req.body;
+
+    const withUserId = { ...user , id : uuidv4()}
+
+    users.push(withUserId);
+    res.send("The User has been pushed" + user.name + "and the age of the user is" + user.age)
+})
+
+router.get('/:id', (req ,res) => {
+
+    const {id} = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+     
+    res.send(foundUser)
+})
+
+router.delete('/:id' , (req, res) => {
+    const {id} = req.params;
+
+    users = users.filter((user)=>{
+        user.id !== id
+    })
+
+    res.send("user Id deleted " + id )
 })
 
 export default router;
